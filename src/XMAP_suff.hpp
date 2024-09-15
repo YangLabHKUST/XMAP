4360ed43480fc7bfe707c6863f8d94d744857931
mxcai <32085863+mxcai@users.noreply.github.com>
2024-09-15 17:21:49 +0800

    Change VI iteration order

--- src/XMAP_suff.hpp
+++ src/XMAP_suff.hpp
@@ -6,6 +6,12 @@ double get_ELBO_XMAP_suff(const arma::cube & XX, const arma::mat & Xy, const arm
                           const arma::colvec & prior_weights,
                           arma::mat gamma, arma::cube mu, arma::field<arma::cube> post_Sig, arma::mat sum_gammamu, arma::mat XXsum_gammamu, 
                           arma::mat nu, arma::mat XXnu, arma::mat Lambda, arma::mat cholinvLambda); 
+double get_ELBO_XMAP_suff_old(const arma::cube & XX, const arma::mat & Xy, const arma::colvec yty, arma::mat & XXtp,
+                          const arma::colvec & n, int K, arma::cube & invSigma, arma::mat Omega, arma::colvec & Sig_E,
+                          const arma::colvec & prior_weights,
+                          arma::mat gamma, arma::cube mu, arma::field<arma::cube> post_Sig, arma::mat sum_gammamu, arma::mat XXsum_gammamu, 
+                          arma::mat nu, arma::mat XXnu, arma::mat Lambda, arma::mat cholinvLambda); 
+
 
 class Timer {
 private:
